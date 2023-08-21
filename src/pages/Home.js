a8901183f923e669f0e9191bc3101638ed9218fc
Ergy Zenzo <ergyzenzo.dev@gmail.com>
2023-08-21 13:03:45 +0200

    home page

---
+++ src/pages/Home.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+import { Box } from '@mui/material'
+
+import Exercises from '../components/Exercises'
+import SearchExercises from '../components/SearchExercises'
+import HeroBanner from '../components/HeroBanner'
+
+const Home = () => {
+  
+  const [exercices, setExercices] = useState([])
+
+  return (
+    <Box>
+      <HeroBanner />
+      <SearchExercises />
+      <Exercises />
+    </Box>
+  )
+}
+
+export default Home
