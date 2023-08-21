9409571763f74fcab133a5abd2ba36f956b41be7
Ergy Zenzo <ergyzenzo.dev@gmail.com>
2023-08-21 16:17:48 +0200

    Added gif and exercise description

---
+++ src/components/Detail.js
@@ -0,0 +1,26 @@
+import { Typography, Stack, Button } from "@mui/material";
+
+import BodyPartImage from '../assets/icons/body-part.png'
+import TargetImage from '../assets/icons/target.png'
+import EquipmentImage from '../assets/icons/equipment.png'
+
+const Detail = ({ exerciseDetail }) => {
+    const { bodyPart, gifUrl, name, target, equipment } = exerciseDetail
+
+
+    return (
+        <Stack gap="60px" sx={{ flexDirection: { lg: 'row' }, p: '20px', alignItems: 'center' }}>
+            <img src={gifUrl} alt={name} loading="lazy" className="detail-image" />
+            <Stack sx={{ gap: { lg: '35px', xs: '20px' } }}>
+                <Typography variant="h3" textTransform='capitalize'>
+                    {name}
+                </Typography>
+                <Typography variant="h6">
+                    Exercise keep you strong. {name} {` `} is one the best exercises to target your {target}. It will help you improve your mood and gain energy.
+                </Typography>
+            </Stack>
+        </Stack>
+    );
+}
+
+export default Detail;
