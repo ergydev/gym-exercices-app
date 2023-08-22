05e99dbc76eb1a368b8b95ebe75d7e92d8ffe2a4
Ergy Zenzo <ergyzenzo.dev@gmail.com>
2023-08-22 12:43:45 +0200

    added similar exercises function

--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -1,8 +1,9 @@
 import { Box, Typography } from "@mui/material"
+import { useContext } from "react"
 import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu'
 
 import BodyPart from "./BodyParts"
-import { useContext } from "react"
+import ExerciseCard from './ExerciseCard'
 
 import RightArrowIcon from '../assets/icons/right-arrow.png'
 import LeftArrowIcon from '../assets/icons/left-arrow.png'
@@ -11,7 +12,7 @@ import LeftArrowIcon from '../assets/icons/left-arrow.png'
 const LeftArrow = () => {
     const { scrollPrev } = useContext(VisibilityContext)
 
-    return(
+    return (
         <Typography onClick={() => scrollPrev()} className="right-arrow">
             <img src={LeftArrowIcon} alt='left-arrow' />
         </Typography>
@@ -21,26 +22,28 @@ const LeftArrow = () => {
 const RightArrow = () => {
     const { scrollNext } = useContext(VisibilityContext)
 
-    return(
+    return (
         <Typography onClick={() => scrollNext()} className="left-arrow">
             <img src={RightArrowIcon} alt='right-arrow' />
         </Typography>
     )
 }
 
-const HorizontalScrollbar = ({ data, bodyPart, setBodyPart }) => {
+const HorizontalScrollbar = ({ data, bodyPart, setBodyPart, isBodyParts }) => {
     return (
         <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
             {data.map((item) => (
-                    <Box
-                        key={item.id || item}
-                        itemId={item.id || item}
-                        title={item.id || item}
-                        m="0 40px"
-                    >
-                        <BodyPart item={item} bodyPart={bodyPart} setBodyPart={setBodyPart} />
-                    </Box>
-                )
+                <Box
+                    key={item.id || item}
+                    itemId={item.id || item}
+                    title={item.id || item}
+                    m="0 40px"
+                >
+                    {isBodyParts ? <BodyPart item={item} bodyPart={bodyPart} setBodyPart={setBodyPart} />
+                        : <ExerciseCard exercise={item} />    
+                    }
+                </Box>
+            )
             )}
         </ScrollMenu>
     )
